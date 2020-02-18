a7e381865c8892d330485c249bf0036e68bb03e5
ellaklara <ellaklara.westerlund@gmail.com>
2020-02-18 18:09:28 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+            <div className="App">
+              <header className="App-header">
+                <Route exact path="/">
+                
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+                </Route>
+                <Route path="/chapter/:nameOfChapter">
+                  <p>
+                    Here is the chapter route!
+                  </p>
+                </Route>
+              </header>
+            </div>
+      </Switch>
+    </Router>
   );
 }
 
