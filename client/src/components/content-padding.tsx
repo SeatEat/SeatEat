4546a16ade3cc9ab0360ee4f15350c53816030ae
= <adamsmajl@gmail.com>
2020-02-28 16:58:36 +0100

    Make content padding expand to full size

--- client/src/components/content-padding.tsx
+++ client/src/components/content-padding.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
 
 const ContentPadding: FC = (props) => {
-    return <div style={{padding: '15px', boxSizing: 'border-box'}}>
+    return <div style={{
+            padding: '15px', 
+            boxSizing: 'border-box',
+            height: '100%',
+            width: '100%'
+        }}>
         {props.children}
     </div>
 }
