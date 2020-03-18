e14be4ceaf07ba6f788e4b271e2f957da8b9798a
= <adamsmajl@gmail.com>
2020-03-18 10:50:33 +0100

    Add code shell for homepage

---
+++ client/src/pages/homepage/homepage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import './homepage.css';
+import ContentPadding from "../../components/content-padding";
+
+const Homepage: FC = (props) => {
+    return <div>
+        <ContentPadding>
+            <h1>Homepage</h1>
+        </ContentPadding>
+    </div>
+}
+
+export default Homepage;
