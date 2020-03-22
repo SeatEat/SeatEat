4d77f080aa214dbcc39bc127bf0f451623463684
= <adamsmajl@gmail.com>
2020-03-22 10:00:53 +0100

    Add disabled option to button compontent

--- client/src/components/button/button.tsx
+++ client/src/components/button/button.tsx
@@ -3,13 +3,17 @@ import './button.css';
 
 interface ButtonProps {
     onClick: Function,
-    isCompact?: boolean
+    isCompact?: boolean,
+    disabled ?: boolean
 }
 
 const Button: FC<ButtonProps> = (props) => {
     return <div 
         onClick={() => props.onClick()} 
-        className={`button ${props.isCompact ? 'button-compact' : ''}`}>
+        className={
+            `button 
+            ${props.isCompact ? 'button-compact' : ''}
+            ${props.disabled ? 'button-disabled' : ''}`}>
         {props.children}
     </div>
 }
