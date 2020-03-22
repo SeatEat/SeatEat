20b5914b22545f3f9218f3fc063832d86ae2da04
= <adamsmajl@gmail.com>
2020-03-22 10:03:55 +0100

    Add error text on input-shell

--- client/src/components/form/input-shell/input-shell.tsx
+++ client/src/components/form/input-shell/input-shell.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import './input-shell.css';
 
 interface InputShellProps {
@@ -13,13 +13,17 @@ const InputShell: FC<InputShellProps> = (props) => {
     return <div className="input-shell-content-wrapper">
         <div className="input-shell-container">
             <div className={`input-shell-description ${props.hasValue ? 'active' : ''}`}>
-                {props.placeholder}
+                {
+                    props.errorText === null
+                    ? props.placeholder
+                    : <div className="input-shell-error-text">{props.errorText}</div>
+                }
             </div>
             {props.children}
         </div>
         <div className="input-shell-border">
             <div className={`input-shell-border-focus ${props.isFocused ? 'active' : ''}`}></div>
-            <div className="input-shell-border-error"></div>
+            <div className={`input-shell-border-error ${props.errorText !== null ? 'active' : ''}`}></div>
         </div>
     </div>
 }
