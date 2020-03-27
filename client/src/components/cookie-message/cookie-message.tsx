8c1f504ee576ff151351800d1de65c0cd5092075
ellaklara <ellaklara.westerlund@gmail.com>
2020-03-27 08:43:12 +0100

    Update cookie msg style

--- client/src/components/cookie-message/cookie-message.tsx
+++ client/src/components/cookie-message/cookie-message.tsx
@@ -1,23 +1,19 @@
 import React, { FC } from 'react';
 import CookieConsent from 'react-cookie-consent';
+import Button from '../button/button';
 import './cookie-message.css';
-
-import Button from "../button/button";
 
 const CookieMessage: FC = () => {
     return (        
         <CookieConsent
-            buttonText="ACCEPT"
+            disableStyles={true}
+            buttonText='OK'
             cookieName="cookie-message"
-            style={{ background: "#fbdc4c", color:"#1b2e63"}}
-            enableDeclineButton
-            declineButtonText="DECLINE (optional)"
-            buttonStyle={{background: "#1b2e63", color: "#fef3ca"}}
-            declineButtonStyle={{background: "#db001e", color: "#fef3ca"}}
-            buttonClasses="button cookie-message-accept"
-            declineButtonClasses="button cookie-message-decline">
-            SeatEat uses cookies to enhance the user experience. Unfortunately not as your dessert. {" "}
-
+            containerClasses='cookie-container'
+            contentClasses='cookie-content'
+            ButtonComponent={Button}
+            >
+            SeatEat uses cookies to enhance the user experience. Unfortunately not as your dessert.
         </CookieConsent>
     );
 }
