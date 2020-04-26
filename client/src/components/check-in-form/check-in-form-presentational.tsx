d2810be170dfcdfd6aca304cdf5e135998de7473
= <adamsmajl@gmail.com>
2020-04-26 14:19:23 +0200

    Split check-in-form into P & C components

---
+++ client/src/components/check-in-form/check-in-form-presentational.tsx
@@ -0,0 +1,56 @@
+import React, { FC, useState, useEffect } from "react";
+import './check-in-form.css';
+
+import Input from "../form/input/input";
+import { ChapterHall } from "../../model/chapter-hall-model";
+import Select from "../form/select/select";
+import Button from "../button/button";
+import { checkInActivities, CheckInActivityIDs } from "../../data/check-in-activities";
+
+interface CheckInFormProps {
+    currentChapter: ChapterHall | null,
+    nameError: string | null,
+    checkInOK: boolean,
+    onNameInputChange: (value: string) => void
+    onActivityInputChange: (value: CheckInActivityIDs) => void,
+    onCancel: () => void,
+    onSubmit: () => void,
+}
+
+const CheckInFormPresentational: FC<CheckInFormProps> = (props) => {
+
+    return (
+        <div>
+            <h2>
+                Check in to {props.currentChapter?.name}
+            </h2>
+            <br/>
+            <Input
+                errorText={props.nameError}
+                placeholder='Full name'
+                onInput={inputValue => props.onNameInputChange(inputValue)}/>
+            <br/>
+            <Select
+                errorText={null}
+                placeholder='Activity'
+                options={checkInActivities.map(checkInReason => {
+                    return {
+                        icon: checkInReason.logo, 
+                        text: checkInReason.title,
+                        value: checkInReason.id
+                    }
+                })}
+                onSelect={value => props.onActivityInputChange(value)}/>
+            <div className="check-in-form-actions">
+                <Button disabled={!props.checkInOK} onClick={() => props.onSubmit()}>
+                    Check in
+                </Button>
+                <Button onClick={() => props.onCancel}>
+                    Cancel
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default CheckInFormPresentational;
--- client/src/components/check-in-form/check-in-form.tsx
+++ client/src/components/check-in-form/check-in-form.tsx
@@ -1,14 +1,12 @@
 import React, { FC, useState, useEffect } from "react";
-import Input from "../form/input/input";
-import { ChapterHall } from "../../model/chapter-hall-model";
-import Select from "../form/select/select";
-import Button from "../button/button";
-
 import './check-in-form.css';
+
+import { ChapterHall } from "../../model/chapter-hall-model";
 import { requestUserCheckIn } from "../../model/redux/checkInState";
 import { connect } from "react-redux";
 import { Dispatch } from "../../model/redux/store";
-import { checkInActivities, CheckInActivityIDs } from "../../data/check-in-activities";
+import { CheckInActivityIDs } from "../../data/check-in-activities";
+import CheckInFormPresentational from "./check-in-form-presentational";
 
 interface CheckInFormDispatch {
     onSubmit: (name: string, type: CheckInActivityIDs) => void
@@ -62,41 +60,18 @@ const CheckInForm: FC<CheckInFormProps & CheckInFormDispatch> = (props) => {
         props.onSubmit(nameValue, typeValue)
     }
 
-    return <div>
-        <h2>
-            Check in to {props.currentChapter?.name}
-        </h2>
-        <br/>
-        <Input
-            errorText={nameError}
-            placeholder='Full name'
-            onInput={inputValue => {
-                setNameValue(inputValue);
-                checkForNameErrors(inputValue);
-            }}/>
-        <br/>
-        <Select
-            errorText={null}
-            placeholder='Activity'
-            options={checkInActivities.map(checkInReason => {
-                return {
-                    icon: checkInReason.logo, 
-                    text: checkInReason.title,
-                    value: checkInReason.id
-                }
-            })}
-            onSelect={value => {
-                setTypeValue(value)
-            }}/>
-        <div className="check-in-form-actions">
-            <Button disabled={!checkInOK} onClick={() => tryCheckIn()}>
-                Check in
-            </Button>
-            <Button onClick={() => props.closeDialog()}>
-                Cancel
-            </Button>
-        </div>
-    </div>
+    return <CheckInFormPresentational
+        checkInOK={checkInOK}
+        currentChapter={props.currentChapter}
+        nameError={nameError}
+        onNameInputChange={(inputValue) => {
+            setNameValue(inputValue);
+            checkForNameErrors(inputValue);
+        }}
+        onActivityInputChange={(value) => setTypeValue(value)}
+        onSubmit={() => tryCheckIn()}
+        onCancel={() => props.closeDialog()}
+    />
 }
 
 export default connect(
