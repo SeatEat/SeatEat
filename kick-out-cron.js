97ee211e4a439a4efaac4bc9f08e44e196edc855
= <adamsmajl@gmail.com>
2020-06-08 12:36:07 +0200

    Fix heroku bug

--- kick-out-cron.js
+++ kick-out-cron.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 const getServerKey = require('./server-key.js');
 let serviceAccount = getServerKey();
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
--- server-key.js
+++ server-key.js
@@ -2,12 +2,12 @@ module.exports = function getServerKey () {
     if (process.env.FIREBASE_SERVER_KEY) {
         return JSON.parse(process.env.FIREBASE_SERVER_KEY);
     }
-    else {
-        var serverKey = "";
-        try {
-            serverKey = require('./firebaseServerKey.json');
-        } catch (error) {
-        }
-        return serverKey;
+
+    try {
+        return require('./firebaseServerKey.json');
+    } catch (error) {
+        console.log('Error!!!!');
+        return '';
     }
+    
 }
