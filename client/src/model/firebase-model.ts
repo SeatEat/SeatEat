d87675b9e0a9267998a094b4c472ec766b933430
= <adamsmajl@gmail.com>
2020-06-08 13:30:09 +0200

    Try again to fix key error

--- client/src/model/firebase-model.ts
+++ client/src/model/firebase-model.ts
@@ -2,8 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 var credentials: any = "";
-if (process.env.FIREBASE_CONFIG) {
-    credentials = JSON.parse(process.env.FIREBASE_CONFIG);
+if (process.env.REACT_APP_FIREBASE_CONFIG) {
+    credentials = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 }
 else {
     credentials = require('../credentials/credentials.json')
